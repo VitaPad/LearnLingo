5b45b7e6c68ed2e086be4342b46bf679a8e6045a
Vita <v.mars22@ukr.net>
2024-10-24 16:42:15 +0300

    rename config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,17 +1,23 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { GoogleAuthProvider } from 'firebase/auth/web-extension';
+
 const API_KEY = import.meta.env.VITE_API_KEY;
+const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
+const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
+const MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
+const APP_ID = import.meta.env.VITE_APP_ID;
 
 const firebaseConfig = {
   apiKey: API_KEY,
-  authDomain: 'learnlingo-f881e.firebaseapp.com',
-  projectId: 'learnlingo-f881e',
-  storageBucket: 'learnlingo-f881e.appspot.com',
-  messagingSenderId: '357186470214',
-  appId: '1:357186470214:web:20f6e7a4ed18bbd9632547',
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
 };
-
+console.log('API_KEY:', API_KEY);
 export const app = initializeApp(firebaseConfig);
 export const googleAuthProvider = new GoogleAuthProvider();
 export const auth = getAuth(app);
