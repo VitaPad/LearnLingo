db2796991c53eac6430f40f1f550a44e54765157
Vita <v.mars22@ukr.net>
2024-11-05 18:47:55 +0200

    add style and svg

--- src/pages/Teachers/Teachers.jsx
+++ src/pages/Teachers/Teachers.jsx
@@ -1,5 +1,6 @@
 /* import Header from '../../components/Header/Header'; */
 import LoadMoreBtn from '../../components/LoadMoreBtn/LoadMoreBtn';
+import SearchForm from '../../components/SearchForm/SearchForm';
 import TeachersList from '../../components/TeachersList/TeachersList';
 import css from './Teachers.module.css';
 
@@ -7,7 +8,12 @@ function Teachers({ theme }) {
   return (
     <div className={css.container}>
       {/*       <Header theme={theme} toggleTheme={toggleTheme} /> */}
-      <TeachersList theme={theme} />
+      <div className={css.searchFormWrapper}>
+        <SearchForm />
+      </div>
+      <div className={css.teachersListWrapper}>
+        <TeachersList theme={theme} />
+      </div>
       <div className={css.btn}>
         <LoadMoreBtn theme={theme} />
       </div>
